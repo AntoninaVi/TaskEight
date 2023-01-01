8d30007c642a16dd5c56be2df5010d4df2623b59
Antonina Vitkalova <you@example.com>
2023-01-01 22:22:44 +0700

    sketch

--- index.js
+++ index.js
@@ -311,3 +311,12 @@ D.addEventListener("DOMContentLoaded", () => {
 
 
 });
+
+setTimeout(function() {
+    $('.message.loading').remove();
+    $('<div class="message new"><figure class="avatar"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure>' + Fake[i] + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    setDate();
+    updateScrollbar();
+    i++;
+  }, 1000 + (Math.random() * 20) * 100);
+
