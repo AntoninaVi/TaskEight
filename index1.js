1c2f2da8f4588f1737656efc04c5fcc46e5cbbe1
Antonina Vitkalova <tonatonotonya@gmail.com>
2023-01-03 22:34:30 +0700

    sketch

--- index1.js
+++ index1.js
@@ -1,8 +1,27 @@
+var know = {
+    "hello": "Hi",
+    "hi": "Good",
+    "john": "hi john"
+    // You Can Add More Stuff Here - It is customizable. - you can add more than 3 
+};
+function talk() {
+    var user = document.getElementById("userBox").value;
+    document.getElementById("chatLog").innerHTML += user + "<br>";
+    if (user in know) {
+        document.getElementById("chatLog").innerHTML += know[user] + "<br>";
+    } else {
+        document.getElementById("chatLog").innerHTML += "I am not a genius , ask me something that I know!<br>";
+    }
+}
 
 
 function displayMessage(type) {
-    var initialMessages = ["Hello friend. What would you like to know?"]
-    var responseMessages = ["Can you please just ask me something I know already?"]
+    var initialMessages = [
+        "open google",
+        "open chrome",
+        "open youtube",
+    ];
+    // var responseMessages = ["FAIL"]
 
     var newDiv = document.createElement("div");
     newDiv.className = "chat-bubble";
@@ -16,22 +35,22 @@ function displayMessage(type) {
 
     var messages = document.getElementById("chat-contents");
     messages.appendChild(newDiv);
-    document.getElementById("isTyping").style.visibility = "hidden";
+    document.getElementById("loaderTyping").style.visibility = "hidden";
 }
 
-function arrowSubmit() {
-    console.log("test")
-    button = document.getElementById("submit-chat");
-    text = document.getElementById("chat-message-value");
+// function arrowSubmit() {
+//     console.log("test")
+//     button = document.getElementById("submit-chat");
+//     text = document.getElementById("chat-message-value");
 
 
-    if (text.value != "") {
-        button.classList = "active";
-    } else {
-        button.classList.remove("active");
+//     if (text.value != "") {
+//         button.classList = "active";
+//     } else {
+//         button.classList.remove("active");
 
-    }
-}
+//     }
+// }
 
 function submitMessage() {
     var text = document.getElementById("chat-message-value").value;
@@ -52,7 +71,7 @@ function submitMessage() {
     var messages = document.getElementById("chat-contents");
     messages.appendChild(newDiv);
     document.getElementById("submit-chat").classList.remove("active");
-    document.getElementById("isTyping").style.visibility = "visible";
+    document.getElementById("loaderTyping").style.visibility = "visible";
     setTimeout(function () { displayMessage("response") }, 5000);
 
 }
@@ -64,10 +83,10 @@ function addHandlers() {
             document.getElementById("submit-chat").click();
         }
     };
-    document.getElementById("isTyping").style.visibility = "hidden";
+    document.getElementById("loaderTyping").style.visibility = "hidden";
 
     setTimeout(function () { displayMessage("initial") }, 5000);
-    document.getElementById("chat-message-value").addEventListener("input", arrowSubmit);
+    // document.getElementById("chat-message-value").addEventListener("input", arrowSubmit);
 }
 
 window.addEventListener("load", addHandlers);
@@ -75,3 +94,19 @@ window.addEventListener("load", addHandlers);
 // const data = new Date(2017, 3, 22, 5, 23, 50);
 // document.getElementById("date1").innerHTML = data;
 
+// const d = new Date();
+// d.setDate(15);
+// document.getElementById("demo").innerHTML = d;
+
+function checkResponse(question) {
+    var questionStr = question.toLowerCase();
+
+    for (var i = 0; i < answersArray.length; i++) {
+        var element = answersArray[i];
+
+        if (questionStr.includes(element.key)) {
+            printAnswer(element.value);
+            return;
+        }
+    }
+}
